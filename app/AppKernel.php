8dad420243809f9e2e82e80793e83f38fd3bae41
piero <piero@piero-ThinkPad-L420>
2012-09-23 02:46:00 -0430

    actualización a Symfony 2.1.2 e intalacion de FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Inass\EleccionBundle\EleccionBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
